4e38b03040645f0811d9ffb269d75549fbdbd485
shahargl <shaharglazner@gmail.com>
2023-11-28 20:57:59 +0200

    feat: wip

--- keep-ui/app/alerts/alert-terraform.tsx
+++ keep-ui/app/alerts/alert-terraform.tsx
@@ -5,6 +5,7 @@ import { Alert } from "./models";
 import Loading from "app/loading"; // Ensure this is the correct path to your Loading component
 import { getApiURL } from "utils/apiUrl";
 import { getSession } from "next-auth/react";
+import { CopyBlock, nord } from "react-code-blocks"; // Import CopyBlock
 
 interface AlertTerraformProps {
   isOpen: boolean;
@@ -20,7 +21,7 @@ export function AlertTerraform({ isOpen, closeModal, alert }: AlertTerraformProp
   useEffect(() => {
     if (!isOpen || !alert) return; // Only fetch when the modal is opened and alert is not null
 
-    if(terraformDefinition) {
+    if (terraformDefinition) {
       return;
     }
     const fetchTerraformDefinition = async () => {
@@ -72,66 +73,66 @@ export function AlertTerraform({ isOpen, closeModal, alert }: AlertTerraformProp
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-50" onClose={closeModal}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-900 bg-opacity-25" />
-        </Transition.Child>
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel
-                className="w-full max-w-4xl transform overflow-hidden ring-tremor bg-white p-6 text-left align-middle shadow-tremor transition-all rounded-xl"
-              >
-                <Title>Terraform Definition</Title>
-                <Divider />
-                {isLoading ? (
-                  <Loading includeMinHeight={false} /> // Display loading indicator
-                ) : (
-                  <div className="mt-4 whitespace-pre-wrap text-sm text-gray-700">
+      <Dialog as="div" className="fixed inset-0 z-50 overflow-y-auto" onClose={closeModal}>
+        <div className="flex items-center justify-center min-h-screen p-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 transition-opacity">
+              <div className="absolute inset-0 bg-gray-900 opacity-75"></div>
+            </div>
+          </Transition.Child>
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+          &#8203;
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="w-full max-w-4xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl sm:w-full sm:max-w-2xl">
+              <Title>Terraform Definition</Title>
+              <Divider />
+              {isLoading ? (
+                <Loading includeMinHeight={false} /> // Display loading indicator
+              ) : (
+                <div className="mt-4">
                     <pre className="whitespace-pre-wrap text-sm text-gray-700">
                       <code>
                         {terraformDefinition || 'No definition available.'}
                       </code>
                     </pre>
-                  </div>
-                )}
-                <Divider />
-                <Flex justifyContent="end" className="mt-4">
-                  <Button
-                    color="orange"
-                    variant="secondary"
-                    onClick={closeModal}
-                  >
-                    Close
-                  </Button>
-                  <Button
-                    color="orange"
-                    className="ml-2"
-                    onClick={downloadTerraformFile}
-                    disabled={isDownloadDisabled}
-                  >
-                    Download
-                  </Button>
-                </Flex>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+                </div>
+              )}
+              <Divider />
+              <Flex justifyContent="end" className="mt-4">
+                <Button
+                  color="orange"
+                  variant="secondary"
+                  onClick={closeModal}
+                >
+                  Close
+                </Button>
+                <Button
+                  color="orange"
+                  className="ml-2"
+                  onClick={downloadTerraformFile}
+                  disabled={isDownloadDisabled}
+                >
+                  Download
+                </Button>
+              </Flex>
+            </div>
+          </Transition.Child>
         </div>
       </Dialog>
     </Transition>
